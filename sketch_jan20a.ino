1525af74671a95404f6608fba489dff354e03fb7
Anand Kumar <77538610+Whitedevil4648@users.noreply.github.com>
2021-01-22 23:09:19 +0530

    Just Nothing

--- sketch_jan20a.ino
+++ sketch_jan20a.ino
@@ -11,7 +11,7 @@ void setup() {
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { 
+  for (pos = 0; pos <= 180; pos += 2) { 
     myservo.write(pos);              
     delay(15);                      
   }
